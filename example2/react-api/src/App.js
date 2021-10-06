ca445d4cbd9c9832c6f7eb1251bacf279ee76ff9
fmauz <fmauz@ipb-halle.de>
2021-10-06 16:17:58 +0200

    package-lock.json, .gitignore und 30 weitere dateien aktualisiert...

---
+++ example2/react-api/src/App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    state = {contacts: [],loading:true}
+
+    render () {
+        const listItems =  this.state.contacts.map((d) => <li key={d.name}>{d.name}</li>);
+       
+        if(!this.state.loading){
+
+            return (<div className="pulsate">{listItems}</div>);
+        }else{
+            return (<div className='pulsate'>Still loading</div>);
+		}
+		
+        
+    }
+    
+    componentDidMount() {
+    setTimeout(()=>{        fetch('http://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ contacts: data,loading:false })
+        })
+        .catch(console.log)},5000);
+        
+
+
+    }
+     
+}
+
+export default App;
---
+++ example2/react-api/src/Script1.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+
+function a(){
+	a=   1;
+}
+
